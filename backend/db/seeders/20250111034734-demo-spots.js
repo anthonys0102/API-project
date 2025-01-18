b54062d2467eb603b6907252d5f65c6e115b846f
anthonys0102 <anthonysantos444@gmail.com>
2025-01-18 14:33:30 -0600

    spot

--- backend/db/seeders/20250111034734-demo-spots.js
+++ backend/db/seeders/20250111034734-demo-spots.js
@@ -1,8 +1,13 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
-  async up(queryInterface) {
-    await queryInterface.bulkInsert('Spots', [
+  async up(queryInterface, Sequelize) {
+    await Spots.bulkCreate([
       {
         ownerId: 1,
         address: '123 Main St',
@@ -45,7 +50,7 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-    ]);
+    ], { validate: true });
   },
 
   async down(queryInterface) {
