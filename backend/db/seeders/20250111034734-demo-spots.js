39d42b8e208c0326efc46769dc2866e994317959
anthonys0102 <anthonysantos444@gmail.com>
2025-01-18 15:32:10 -0600

    spots

--- backend/db/seeders/20250111034734-demo-spots.js
+++ backend/db/seeders/20250111034734-demo-spots.js
@@ -4,10 +4,9 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
 }
-
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await Spots.bulkCreate([
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('Spots', [
       {
         ownerId: 1,
         address: '123 Main St',
@@ -50,14 +49,10 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date(),
       },
-    ], { validate: true });
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-
-    }, {});
-  }
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Spots', null, {});
+  },
 };
